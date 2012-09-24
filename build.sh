2f4d3b180332135747a3c9129cbc96c41f7bb4f8
Saam Barati <saambarati1@gmail.com>
2012-09-24 00:34:00 -0700

    remove old files

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm ./data.json ;  node ./cli.js ;   echo created blank data.json #basically get a blank data.json again
--- postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -e "./data.json" ]
-then
-  out=$(node ./cli.js -l 2>&1) #silent stdout while creating ./data.json file
-  echo "creating data.json file"
-else
-  echo "not overwriting globals in data.json"
-fi
