e763ad0cc3242e135b51be9745c99420a11e9a1d
Saam Barati <saambarati1@gmail.com>
2012-09-23 21:08:16 -0700

    postuninstall doesn't really work. added better error messages for removing data file

--- postuninstall.js
+++ postuninstall.js
@@ -10,4 +10,5 @@ try {
   console.log('successfuly removed data file at location: ' + DATA_FILE.green)
 } catch (e) {
   console.log('could not remove file at file path ' + DATA_FILE.red + '\nError: ' + e.message.red)
+  console.log('\nAccess was probably denied. \nI would recommend running: ' + ('$( rm ' + DATA_FILE + ' )').green + '  from your terminal ')
 }
